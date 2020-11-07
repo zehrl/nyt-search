b1a228eeb7a031b22ac4e7cd9c752e9b9923b35c
Logan Zehr <zehrl315@outlook.com>
2020-11-07 13:44:00 -0800

    Javascript values from form added

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,19 @@
 
 
 $("#search-term").val();
+
+$("#search-parameters").submit(function(){
+
+    // Error check input
+
+    // Pull values of each element
+    var searchTerm = $("#search-term").val();
+    console.log(searchTerm);
+
+    var numberOfRecords = $("#number-of-records").val();
+    var startYear = $("#start-year").val();
+    var endYear = $("#end-year").val();
+
+    console.log(searchTerm, numberOfRecords, startYear, endYear);
+
+})
